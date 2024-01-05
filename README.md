249695683bf65303927199bfdde6df76b070643f
Katarya Johnson-Williams <kajohnsonwilliam3168@eagle.fgcu.edu>
2024-01-05 14:32:47 -0500

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# HoneyKat11.github.io
-The purpose of this project is to create a personal website to showcase my resume and other important information about myself.
---
+++ precache-manifest.384fa7546451643cce97ddb259790d16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fc20315e3270270624b8f9d0226af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1633940d48fcaab79e",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "43eb42f034994aab6a91",
+    "url": "/static/js/2.c25185bb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.c25185bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93eb3beb917277995f2",
+    "url": "/static/js/3.34c34796.chunk.js"
+  },
+  {
+    "revision": "6b1633940d48fcaab79e",
+    "url": "/static/js/main.7a8974a6.chunk.js"
+  },
+  {
+    "revision": "25d9607456ff2ca34fe0",
+    "url": "/static/js/runtime-main.2032bf83.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
